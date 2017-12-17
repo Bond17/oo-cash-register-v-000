87047af58dc13b3f082366cddf6e182accf0eb9e
Robert Watson <spartan83@gmail.com>
2017-12-17 21:02:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ def apply_discount
 if discount == 0
   return "There is no discount to apply."
 else
-  self.total = self.total-self.discount
+  self.total = self.total+self.discount
   return "discount applied"
 end
 end
